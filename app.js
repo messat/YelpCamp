a872623dad933b500ed550799655e6f6a6b2fc82
messat <148489126+messat@users.noreply.github.com>
2024-10-01 16:08:09 +0100

    This commit message adds new API GET AND POST routes to display all campgrounds and add new campground

--- app.js
+++ app.js
@@ -30,15 +30,36 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 
+app.use(express.urlencoded({ extended: true}))
+
+
+
 
 app.get('/', (req,res)=>{
     res.render('home')
 })
 
-app.get('/makecampground', async (req,res)=>{
-    const camp = new Campground({title: "My Backyard"})
-    await camp.save()
-    res.send(camp)
+app.get('/campgrounds', async (req,res) => {
+    const campgrounds = await Campground.find({})
+    res.render('campgrounds/index', {campgrounds})
+})
+
+
+app.get('/campgrounds/new', (req,res)=>{
+    res.render('campgrounds/form')
+})
+
+
+app.get('/campgrounds/:id', async (req, res) => {
+    const {id} =req.params
+    const campground = await Campground.findById(id)
+    res.render('campgrounds/show', {campground})
+})
+
+app.post('/campgrounds', async (req,res)=>{
+    const newCampground = new Campground(req.body.campground)
+    await newCampground.save()
+    res.redirect(`/campgrounds/${newCampground._id}`)
 })
 
 
