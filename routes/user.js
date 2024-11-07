193c93c8fca950a78e13cf0891be30e97c9b55e3
messat <148489126+messat@users.noreply.github.com>
2024-11-07 16:03:57 +0000

    This commit adds login/logout link on the navbar and uses passport logout function. Added memory when user is prompted with login, upon successful the user is returned to original page visited

--- routes/user.js
+++ routes/user.js
@@ -3,18 +3,24 @@ const router = express.Router()
 const User = require('../models/user')
 const catchAsync = require('../utils/catchAsync')
 const passport = require('passport')
+const {storeReturnTo} = require('../validateMiddleware')
 
 router.get('/register', (req, res)=>{
     res.render('users/register')
 })
 
-router.post('/register', catchAsync(async (req,res) => {
+router.post('/register', catchAsync(async (req,res, next) => {
     try {
         const { email, username, password } = req.body
         const newUser = new User({email, username})
         const registeredUser =  await User.register(newUser, password)
-        req.flash("success", `Welcome ${username} to Yelp camp! Thank you for registering`)
-        res.redirect('/campgrounds')
+        req.login(registeredUser, function(err) {
+            if (err) { 
+                return next(err); 
+                }
+            req.flash("success", `Welcome ${username} to Yelp camp! Thank you for registering`)
+            res.redirect('/campgrounds')
+          });
     } catch (e){
         req.flash('error', e.message)
         res.redirect('/register')
@@ -25,10 +31,19 @@ router.get('/login', (req, res)=>{
     res.render('users/login')
 })
 
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login'}), async (req,res) => {
+router.post('/login', storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login'}), async (req,res) => {
     const {username} = req.body
     req.flash('success', `Welcome back, ${username}!`)
-    res.redirect('/campgrounds')
+    const redirectUrl = res.locals.returnTo || '/campgrounds'
+    res.redirect(redirectUrl)
+})
+
+router.get('/logout', (req,res)=>{
+    req.logout(function(err) {
+        if (err) { return next(err); }
+        req.flash('success', 'You are logged out! Goodbye')
+        res.redirect('/campgrounds');
+      });
 })
 
 
