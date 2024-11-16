7834ba8b8568bd4f84459c9e3eeec5f29f19b4e3
messat <148489126+messat@users.noreply.github.com>
2024-11-16 22:11:30 +0000

    This commit adds permissions to edit the campground by the original author only. Created middleware function to validate author on edit and delete routes of campground

--- routes/review.js
+++ routes/review.js
@@ -3,21 +3,10 @@ const router = express.Router({ mergeParams: true})
 
 const Campground = require('../models/campground.js');
 const Review = require('../models/reviews.js')
-
-const { reviewSchema} = require("../schemas.js")
-
+const {validateReview} = require('../validateMiddleware.js')
 const catchAsync = require('../utils/catchAsync')
-const ExpressError = require("../utils/ExpressError");
 
-const validateReview = (req,res, next)=>{
-    const {error} = reviewSchema.validate(req.body)
-    if(error){
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next()
-    }
-}
+
 
 router.post('/', validateReview, catchAsync(async (req,res) => {
     const {id} = req.params
--- validateMiddleware.js
+++ validateMiddleware.js
@@ -1,3 +1,7 @@
+const ExpressError = require('./utils/ExpressError');
+const {campgroundSchema, reviewSchema} = require('./schemas');
+const Campground = require('./models/campground')
+
 module.exports.isLoggedIn = (req, res, next) =>{
     if (!req.isAuthenticated()) {
         req.session.returnTo = req.originalUrl
@@ -13,4 +17,34 @@ module.exports.storeReturnTo = (req,res, next) =>{
         res.locals.returnTo = req.session.returnTo
     } 
     next()
+}
+
+module.exports.validateCampground = (req,res,next) =>{
+    const {error } = campgroundSchema.validate(req.body)
+    if(error){
+        const msg = error.details.map(el => el.message).join(",")
+        throw new ExpressError(msg, 400)
+    } else {
+        next()
+    }
+}
+
+module.exports.verifyAuthor = async (req,res,next) => {
+    const {id} = req.params
+    const findCampground = await Campground.findById(id)
+    if (!findCampground.author.equals(req.user._id)){
+        req.flash('error', 'You do not have permission to do that')
+        return res.redirect(`/campgrounds/${id}`)
+    }
+    next()
+}
+
+module.exports.validateReview = (req,res, next)=>{
+    const {error} = reviewSchema.validate(req.body)
+    if(error){
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next()
+    }
 }
