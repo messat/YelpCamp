d7b870cebd45c4298f4c28f4d82e9608178ddc7b
messat <148489126+messat@users.noreply.github.com>
2024-11-16 23:10:27 +0000

    This commit adds middleware function to protect review post/delete routes from postman

--- routes/campground.js
+++ routes/campground.js
@@ -18,7 +18,12 @@ router.get('/new', isLoggedIn, (req,res)=>{
 
 router.get('/:id', catchAsync(async (req, res) => {
     const {id} =req.params
-    const campground = await Campground.findById(id).populate('reviews').populate('author')
+    const campground = await Campground.findById(id).populate({
+        path: 'reviews', 
+        populate: {
+        path: 'author'
+        }
+    }).populate('author')
     if(!campground){
         req.flash('error', 'Cannot find that campground!')
         return res.redirect('/campgrounds')
