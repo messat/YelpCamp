7d2fb426fc118cca5b51fdead2c489dd80ed7b1c
messat <148489126+messat@users.noreply.github.com>
2024-11-17 15:57:47 +0000

    This commit adds controller folder and use the MVC principles to keep the routes compact for campground, reviews, and users

--- routes/review.js
+++ routes/review.js
@@ -1,31 +1,12 @@
 const express= require('express')
 const router = express.Router({ mergeParams: true})
 
-const Campground = require('../models/campground.js');
-const Review = require('../models/reviews.js')
 const {validateReview, isLoggedIn, isReviewAuthor} = require('../validateMiddleware.js')
-const catchAsync = require('../utils/catchAsync')
+const catchAsync = require('../utils/catchAsync');
+const { createReviews, deleteReviews } = require('../controller/reviews.js');
 
+router.post('/', isLoggedIn, validateReview, catchAsync(createReviews))
 
-
-router.post('/', isLoggedIn, validateReview, catchAsync(async (req,res) => {
-    const {id} = req.params
-    const campground = await Campground.findById(id)
-    const review = new Review(req.body.review)
-    review.author = req.user._id
-    campground.reviews.push(review)
-    await review.save()
-    await campground.save()
-    req.flash('success', 'Posted new review!')
-    res.redirect(`/campgrounds/${campground._id}`)
-}))
-
-router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(async (req,res) => {
-    const {id, reviewId} = req.params
-    await Campground.findByIdAndUpdate(id, {$pull: {reviews: reviewId}})
-    await Review.findByIdAndDelete(reviewId)
-    req.flash('success', 'Successfully deleted review!')
-    res.redirect(`/campgrounds/${id}`)
-}))
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(deleteReviews))
 
 module.exports = router
--- routes/user.js
+++ routes/user.js
@@ -1,52 +1,19 @@
 const express = require('express')
 const router = express.Router()
-const User = require('../models/user')
 const catchAsync = require('../utils/catchAsync')
 const passport = require('passport')
 const {storeReturnTo} = require('../validateMiddleware')
+const { renderRegisterForm, createNewUser, renderLogin, login, logout } = require('../controller/user')
 
-router.get('/register', (req, res)=>{
-    res.render('users/register')
-})
+router.route('/register')
+    .get(renderRegisterForm)
+    .post(catchAsync(createNewUser))
 
-router.post('/register', catchAsync(async (req,res, next) => {
-    try {
-        const { email, username, password } = req.body
-        const newUser = new User({email, username})
-        const registeredUser =  await User.register(newUser, password)
-        req.login(registeredUser, function(err) {
-            if (err) { 
-                return next(err); 
-                }
-            req.flash("success", `Welcome ${username} to Yelp camp! Thank you for registering`)
-            res.redirect('/campgrounds')
-          });
-    } catch (e){
-        req.flash('error', e.message)
-        res.redirect('/register')
-    }
-}))
+router.route('/login')
+    .get(renderLogin)
+    .post(storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login'}), login)
 
-router.get('/login', (req, res)=>{
-    res.render('users/login')
-})
-
-router.post('/login', storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login'}), async (req,res) => {
-    const {username} = req.body
-    req.flash('success', `Welcome back, ${username}!`)
-    const redirectUrl = res.locals.returnTo || '/campgrounds'
-    delete req.session.returnTo
-    res.redirect(redirectUrl)
-})
-
-router.get('/logout', (req,res)=>{
-    req.logout(function(err) {
-        if (err) { return next(err); }
-        req.flash('success', 'You are logged out! Goodbye')
-        res.redirect('/campgrounds');
-      });
-})
-
-
+router.route('/logout')
+    .get(logout)
 
 module.exports = router
