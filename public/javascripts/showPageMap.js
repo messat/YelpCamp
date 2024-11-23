97f7d72e890ecf52b4b446c020e3d5e1db29b852
messat <148489126+messat@users.noreply.github.com>
2024-11-23 17:53:39 +0000

    This commit finds the campground location coordinates on the map. Also I have added marker to the selected destination

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,7 +1,13 @@
+const coordinatesFinder = JSON.parse(campground).geometry.coordinates
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: 'map', // container ID
   style: 'mapbox://styles/mapbox/streets-v12', // style URL
-  center: [-74.5, 40], // starting position [lng, lat]
-  zoom: 4, // starting zoom
+  center: coordinatesFinder, // starting position [lng, lat]
+  zoom: 10, // starting zoom
 });
+
+//pin - marker
+new mapboxgl.Marker()
+  .setLngLat(coordinatesFinder)
+  .addTo(map)
