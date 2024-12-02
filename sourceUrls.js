731421d5f8c248331944988688560793e9ac0f27
messat <148489126+messat@users.noreply.github.com>
2024-12-02 22:58:16 +0000

    Changed the directives on helmet to allow mapbox scripts to work during production

--- sourceUrls.js
+++ sourceUrls.js
@@ -2,11 +2,16 @@ const scriptSrcUrls = [
     "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js",
     "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js",
     "https://api.mapbox.com/mapbox-gl-js/v3.8.0/mapbox-gl.js",
+    "https://yelpcamp-ch30.onrender.com", 
+    "https://api.mapbox.com",
+    "https://events.mapbox.com"
 ]
 
 const styleSrcUrls = [
     "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css",
     "https://api.mapbox.com/mapbox-gl-js/v3.8.0/mapbox-gl.css",
+    "https://api.mapbox.com",
+    "https://fonts.googleapis.com"
 ]
 
 const connectSrcUrls = [
@@ -14,6 +19,8 @@ const connectSrcUrls = [
     "https://events.mapbox.com"
 ]
 
-const fontSrcUrls = []
+const fontSrcUrls = [
+    "https://fonts.gstatic.com"
+]
 
 module.exports = { scriptSrcUrls, styleSrcUrls, connectSrcUrls, fontSrcUrls}
